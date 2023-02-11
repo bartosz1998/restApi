81456438fc2a14995b9a95ef65641f95a46df7bb
bartosz <bartoszbizon1998@gmail.com>
2023-02-11 13:22:47 +0100

    trying adding swagger 2

--- src/main/java/com/example/restapi/config/Config.java
+++ src/main/java/com/example/restapi/config/Config.java
@@ -3,15 +3,38 @@ package com.example.restapi.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class Config {
+@EnableSwagger2
+public class Config implements WebMvcConfigurer {
 
-    @Autowired
-    private ObjectMapper objectMapper;
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    void customizeObjectMapper(){
+//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+//    }
 
-    void customizeObjectMapper(){
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
---
+++ src/main/java/com/example/restapi/model/Post.java
@@ -0,0 +1,18 @@
+package com.example.restapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+@Entity
+@Getter
+@Setter
+public class Post {
+    @Id
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime created;
+}
