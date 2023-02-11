00eadb28a61051212d0b2141b7c43c02ef133b46
bartosz <bartoszbizon1998@gmail.com>
2023-02-11 17:10:45 +0100

    return data

--- src/main/java/com/example/restapi/model/Comment.java
+++ src/main/java/com/example/restapi/model/Comment.java
@@ -5,22 +5,14 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class Post {
+public class Comment {
     @Id
-    private Long id;
-    private String title;
+    private long id;
     private String content;
     private LocalDateTime created;
-
-    @OneToMany
-    @JoinColumn(name = "post_id")
-    private List<Comment> comment;
 }
