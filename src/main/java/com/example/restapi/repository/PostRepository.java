6aa70921715aa6d765c59618149a3798716006bb
bartosz <bartoszbizon1998@gmail.com>
2023-02-11 17:28:37 +0100

    one method for method spring data

--- src/main/java/com/example/restapi/repository/PostRepository.java
+++ src/main/java/com/example/restapi/repository/PostRepository.java
@@ -2,9 +2,14 @@ package com.example.restapi.repository;
 
 import com.example.restapi.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    @Query("select p from Post p where title = :title")
+    List<Post> findAllByTitle(String title);
 }
